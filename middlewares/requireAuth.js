bad5139b7dd0506db233d722bfa03014ef39e769
Michel <contact@michelcurti.dev>
2020-01-16 16:22:47 -0300

    check for autorization token

--- middlewares/requireAuth.js
+++ middlewares/requireAuth.js
@@ -7,6 +7,9 @@ module.exports = (req, res, next) => {
 
     const { authorization } = req.headers;
 
+    if(!authorization)
+        return res.status(401).send({ error : "You must be logged in!" });
+
     const token = authorization.replace('Bearer ', "");
 
     jwt.verify(token, "MY_SECRET_GOES_HERE", async (err, payload) => {
